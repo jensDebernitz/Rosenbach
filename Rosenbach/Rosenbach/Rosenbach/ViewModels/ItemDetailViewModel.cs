d1faa2213a7061456c8eb8ae0cac671112479658
jens.debernitz <jens.debernitz@cgm.com>
2020-02-01 14:58:40 +0100

    initial commit

---
+++ Rosenbach/Rosenbach/Rosenbach/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Rosenbach.Models;
+
+namespace Rosenbach.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
